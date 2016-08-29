a8cbb7645580d7e9d3804d4bebfce36310124bf3
Jme <John.Solomon@procare.co.nz>
2016-08-29 12:04:44 +1200

    palindrome checker

---
+++ console/ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Check("abcba", true);
+            Check("abcde", false);
+            Check("Mr owl ate my metal worm", true);
+            Check("Never Odd Or Even", true);
+            Check("Never Even Or Odd", false);
+
+            Console.ReadKey(); //to exit
+        }
+
+        public static void Check(string s, bool shouldBePalindrome)
+        {
+            Console.WriteLine(IsPalindrome(s) == shouldBePalindrome ? "pass" : "FAIL");
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            var index = 0;
+            var lengthOfString = s.Length - 1;
+
+            while (index < lengthOfString) 
+            {
+                if (s[index] != s[lengthOfString])
+                {
+                    return false;
+                }
+
+                index ++;
+                lengthOfString--;
+            }
+
+            return true;
+        }
+    }
+}
