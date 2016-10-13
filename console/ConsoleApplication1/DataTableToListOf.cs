c8e0be95bf37c56eaf40415c9bcee5fa5607d748
PROCAREHO\John.Solomon <John.Solomon@procare.co.nz>
2016-10-13 17:12:12 +1300

    http parser and datatabletolistofobjects

---
+++ console/ConsoleApplication1/DataTableToListOf.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class DataTableHelper
+    {
+        // <summary>
+        /// try to convert data table to a list of objects
+        /// </summary>
+        /// <typeparam name="T">Generic object</typeparam>
+        /// <param name="table">DataTable</param>
+        /// <returns>List with generic objects</returns>
+        public static List<T> DataTableToListOf<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                var list = new List<T>();
+
+                foreach (var row in table.AsEnumerable())
+                {
+                    T obj = new T();
+
+                    foreach (var prop in obj.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+                            propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
