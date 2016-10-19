15b3ef99c1d9569e56567910af1d9912425efde4
PROCAREHO\John.Solomon <John.Solomon@procare.co.nz>
2016-10-20 10:46:54 +1300

    removed unused ref

--- console/ConsoleApplication1/Program.cs
+++ console/ConsoleApplication1/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
